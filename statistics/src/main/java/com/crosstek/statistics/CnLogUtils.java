514521a24e57e8e9539379279df241197e449e82
yemint <yemint@outlook,com>
2018-12-11 15:58:48 +0800

    修改Log.public

--- statistics/src/main/java/com/crosstek/statistics/CnLogUtils.java
+++ statistics/src/main/java/com/crosstek/statistics/CnLogUtils.java
@@ -16,38 +16,17 @@ public class CnLogUtils {
     public static boolean isDebug = AppConstants.isDebug;// 是否需要打印bug，可以在application的onCreate函数里面初始化
     private static final String TAG = "CnLogUtils:";
 
-//    // 下面四个是默认tag的函数
-//    public static void i(String msg) {
-//        if (isDebug)
-//            Log.i(TAG, msg);
-//    }
-//
-//    public static void d(String msg) {
-//        if (isDebug)
-//            Log.d(TAG, msg);
-//    }
-//
-//    public static void e(String msg) {
-//        if (isDebug)
-//            Log.e(TAG, msg);
-//    }
-//
-//    public static void v(String msg) {
-//        if (isDebug)
-//            Log.v(TAG, msg);
-//    }
-
-    static void i(String methodName, String msg) {
+    public static void i(String methodName, String msg) {
         if (isDebug)
             Log.i(TAG, canvs(methodName, msg));
     }
 
-    static void d(String methodName, String msg) {
+    public static void d(String methodName, String msg) {
         if (isDebug)
             Log.d(TAG, canvs(methodName, msg));
     }
 
-    static void e(String methodName, String msg) {
+    public static void e(String methodName, String msg) {
         if (isDebug)
             Log.e(TAG, canvs(methodName, msg));
     }
